4c739f3f2a24b56014598e3c4aa4c0b210e1eed9
James Daniel Cadavona <jdpcadavona@gmail.com>
2025-04-22 20:14:29 -0700

    working on auth.tsx

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,31 +1,36 @@
 // app/index.tsx
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
+import { useFonts } from 'expo-font';
 import { supabase } from '../services/supabaseClient';
 import { router } from 'expo-router';
 
 export default function Index() {
   const [checking, setChecking] = useState(true);
 
-  useEffect(() => {
+const [fontsLoaded] = useFonts({
+    Outfit: require('../assets/fonts/Outfit-VariableFont_wght.ttf'),
+})
+
+useEffect(() => {
     const checkUser = async () => {
       const { data } = await supabase.auth.getSession();
 
       if (data.session) {
-        // ✅ User is logged in — go to home/dashboard (or whatever you call it later)
         router.replace('/home');
       } else {
-        // 👤 No user, go to auth screen
         router.replace('/auth');
       }
 
       setChecking(false);
     };
 
-    checkUser();
-  }, []);
+    if (fontsLoaded) {
+      checkUser();
+    }
+  }, [fontsLoaded]);
 
-  if (checking) {
+  if (!fontsLoaded ||checking) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" />
